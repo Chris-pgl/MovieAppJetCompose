09f1c55c7188ff31f5c75a2b6f79ce8723efde3b
Chris <cr1s@hotmail.it>
2026-05-06 16:03:41 +0200

    add LazyColum for movies

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movieapp.ui.theme.MovieAppTheme
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,38 +27,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyApp(modifier = Modifier.padding(innerPadding)) {
-                        Text("Contenuto della Movie App")
+                    MyApp{
+                        MainContent()
                     }
                 }
+            }
+        }
+    }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier, 
+    content: @Composable () -> Unit
+){
+    MovieAppTheme{
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Movie App")},
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0xFF2196F3)
+                    )
+                )
+            }
+        ){innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding)
+            ){
+                content()
             }
         }
     }
 }
 
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier, 
-    content: @Composable () -> Unit
-){
-    content()
-}
-
-@Composable
-fun MainContent(){
-    Scaffold(containerColor = MaterialTheme.colorScheme.background) {
-        innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            Text("MainContent")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life",
+        "Matrix",
+        "Inception"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(movieList) { movie ->
+                Text(text = movie)
+            }
         }
     }
 }
+
+
 
 @Composable
 @Preview(showBackground = true)
 fun MyAppPreview() {
     MyApp() {
-        Text("Contenuto della Movie AppPre")
+        MainContent()
     }
 }
